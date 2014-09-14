afe18da46e6c15ef1a4ebeaa4d25ba4c945888b5
Tihomir Dimov <tihomirdimov92@gmail.com>
2014-09-14 11:50:53 +0300

    Login on server ready

--- server/config/express.js
+++ server/config/express.js
@@ -1,12 +1,19 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport');
 
 module.exports = function (app, config) {
     app.set('view engine', 'jade'); // set view engine to express
     app.set('views', config.rootPath + '/server/views'); // set folder where are views
     app.use(express.static(config.rootPath + '/public')); // set path to static files
+    app.use(cookieParser());
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
+    app.use(session());
+    app.use(passport.initialize());
+    app.use(passport.session());
 };
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,6 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalPassport = require('passport-local');
 
 module.exports = function (config){
     mongoose.connect(config.db);
@@ -15,5 +17,65 @@ module.exports = function (config){
 
     db.on('error', function (err) {
         console.log(err);
+    });
+
+    var userSchema = new mongoose.Schema({
+        username: String,
+        firstName: String,
+        lastName: String
+        //salt: String,
+        //passHash: String
+    })
+
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection) {
+        if(err) {
+            console.log('Cannot find users: ' + err);
+            return;
+        }
+
+        if(collection.length === 0) {''
+            User.create({username: 'tihomir.dimov', firstName: 'Tihomir', lastName: 'Dimov'});
+            User.create({username: 'vladimir.dimov', firstName: 'Vladimir', lastName: 'Dimov'});
+            console.log('Users addded to database');
+        }
+    });
+
+    passport.use(new LocalPassport(function (username, password, done){
+        User.findOne({username: username}).exec(function (err, user) {
+            if(err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            if(user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+
+        });
+    }));
+
+    passport.serializeUser(function (user, done){
+       if(user) {
+           return done(null, user._id);
+       }
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findOne({_id: id}).exec(function (err, user) {
+            if(err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            if(user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
     });
 };
